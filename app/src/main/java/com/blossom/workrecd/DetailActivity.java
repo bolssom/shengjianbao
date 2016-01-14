c99995f7e6d11ca7c52c871f0b461159537ee5a1
zxw <147436405@qq.com>
2016-01-14 17:33:43 +0800

    201601114

--- app/src/main/java/com/blossom/workrecd/DetailActivity.java
+++ app/src/main/java/com/blossom/workrecd/DetailActivity.java
@@ -1,16 +1,20 @@
 package com.blossom.workrecd;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.blossom.workrecd.Utils.ToastHelper;
 import com.lidroid.xutils.ViewUtils;
@@ -25,7 +29,7 @@ public class DetailActivity extends Activity {
     private RadioButton radio_qy;
     @ViewInject(R.id.layout_container)
     private  LinearLayout container;
-
+private LinearLayout xianzaibaoming;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,17 +44,70 @@ public class DetailActivity extends Activity {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if (radio_zw.getId() == checkedId) {
-                   container.removeAllViews();
+                    container.removeAllViews();
                     container.addView(View.inflate(DetailActivity.this, R.layout.detail_zhiwei, null));
                 } else if (radio_qy.getId() == checkedId) {
-                  container.removeAllViews();
+                    container.removeAllViews();
                     LayoutInflater lf = LayoutInflater.from(DetailActivity.this);
-                    container.addView(lf.inflate(R.layout.detail_qiye,null));
+                    container.addView(lf.inflate(R.layout.detail_qiye, null));
                 }
             }
         });
         radio_zw.setChecked(true);
+        xianzaibaoming = (LinearLayout)container.findViewById(R.id.xianzaibaoming);
+        xianzaibaoming.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customdialog();
+            }
+        });
+
     }
+
+    private void customdialog() {
+        LayoutInflater inflaterDl = LayoutInflater.from(this);
+        RelativeLayout layout = (RelativeLayout)inflaterDl.inflate(R.layout.dialog_detail, null );
+
+        //对话框
+        final Dialog dialog = new AlertDialog.Builder(DetailActivity.this).create();
+        dialog.show();
+        dialog.getWindow().setContentView(layout);
+        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+
+//        //取消按钮
+//        Button btnCancel = (Button) layout.findViewById(R.id.dialog_close);
+//        btnCancel.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "cancel", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+
+        //确定按钮
+        Button btnOK = (Button) layout.findViewById(R.id.dialog_ok);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "ok", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        //关闭按钮
+        ImageButton btnClose = (ImageButton) layout.findViewById(R.id.dialog_close);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+
     @OnClick({R.id.left_btn,R.id.right_btn})
     public void myClick(View v){
         switch (v.getId()){
@@ -60,6 +117,10 @@ public class DetailActivity extends Activity {
             case R.id.right_btn:
                 ToastHelper.show(this, "导出");
                 break;
+//            case  R.id.xianzaibaoming:
+//                System.out.println("对话框------------");
+//                customdialog();
+//                break;
         }
     }
 }

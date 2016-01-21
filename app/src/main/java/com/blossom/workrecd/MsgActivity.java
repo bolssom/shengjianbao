b132f7871e0ddc958f231f46d9a9dabc62609212
zxw <147436405@qq.com>
2016-01-21 23:28:15 +0800

    2016-01-21

--- app/src/main/java/com/blossom/workrecd/MsgActivity.java
+++ app/src/main/java/com/blossom/workrecd/MsgActivity.java
@@ -9,15 +9,21 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.blossom.workrecd.Adapter.DataAdapter;
 import com.blossom.workrecd.Utils.ToastHelper;
 import com.blossom.workrecd.View.TitleView;
+import com.blossom.workrecd.View.swipelistview.BaseSwipeListViewListener;
+import com.blossom.workrecd.View.swipelistview.SwipeListView;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MsgActivity extends Activity {
     @ViewInject(R.id.title)
     private TitleView mtitleview;
-    private  RadioGroup clientRadioGroup;
+    private RadioGroup clientRadioGroup;
     @ViewInject(R.id.radio_z)
     private RadioButton radio_z;
     @ViewInject(R.id.radio_y)
@@ -26,6 +32,11 @@ public class MsgActivity extends Activity {
     private LinearLayout l1;
     @ViewInject(R.id.layout2)
     private LinearLayout l2;
+
+    SwipeListView swipeListView;
+    List<String> mDatas;
+    DataAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,31 +44,56 @@ public class MsgActivity extends Activity {
         setContentView(R.layout.activity_msg);
         ViewUtils.inject(this);
         initview();
+        mDatas = new ArrayList<String>();
+        for (int i = 0; i < 20; i++) {
+            mDatas.add("记录" + i);
+        }
+        swipeListView = (SwipeListView) findViewById(R.id.example_lv_list);
+        adapter = new DataAdapter(this, mDatas, swipeListView);
+        swipeListView.setAdapter(adapter);
 
-        clientRadioGroup = (RadioGroup) findViewById(R.id.msg_radio);
-        clientRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        swipeListView.setSwipeListViewListener(new BaseSwipeListViewListener() {
+            // 这里可以重写很多方法
+            @Override
+            public void onListChanged() {
+
+                super.onListChanged();
+
+            }
 
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
+            public void onClickFrontView(int position) {
+
+                super.onClickFrontView(position);
+
+            }
+
+        });
+
+    clientRadioGroup = (RadioGroup) findViewById(R.id.msg_radio);
+            clientRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
 //                int radioButtonId = group.getCheckedRadioButtonId();
 //                RadioButton rb = (RadioButton) findViewById(radioButtonId);
 //                String radioButtonLabel = rb.getText().toString();
-                if (radio_z.getId() == checkedId){
-                   l1.setVisibility(View.VISIBLE);
-                    l2.setVisibility(View.GONE);
-                }else if (radio_y.getId() == checkedId){
-                    l2.setVisibility(View.VISIBLE);
-                    l1.setVisibility(View.GONE);
+                    if (radio_z.getId() == checkedId) {
+                        l1.setVisibility(View.VISIBLE);
+                        l2.setVisibility(View.GONE);
+                    } else if (radio_y.getId() == checkedId) {
+                        l2.setVisibility(View.VISIBLE);
+                        l1.setVisibility(View.GONE);
+                    }
                 }
-            }
-        });
-       // radio_z.setChecked(true);
-    }
+            });
+            // radio_z.setChecked(true);
+        }
 
     private void initview() {
 
         mtitleview.setTitle("消息");
-        mtitleview.setLeftImageButton( new TitleView.OnLeftImageButtonClickLister() {
+        mtitleview.setLeftImageButton(new TitleView.OnLeftImageButtonClickLister() {
             @Override
             public void onClick(View button) {
                 finish();
